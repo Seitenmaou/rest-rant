ce5bcde8141f9b22e715e3567126dcd7f1f45677
Euri Tanaka <dev@mugendiary.com>
2022-05-29 11:33:01 -0700

    Adding places controller

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+//Router
+app.use('/places', require('./controllers/places.js'))
+
+//Main Page
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
+//404
 app.get('*', (req, res) => {
     res.status(404).sernd('<h1>404 Page</h1>')
 })
 
+//Listen
 app.listen(process.env.PORT)
