cadfc54eff50262bc10b3b20b2a70b97f984c074
Euri Tanaka <dev@mugendiary.com>
2022-06-05 16:14:21 -0700

    mongoose implementation

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -40,6 +40,10 @@ function new_form (){
                         name="cuisines"
                         required/>
                     </div>
+                    <div className="form-group">
+                        <label for="founded">Founded Year</label>
+                        <input className="form-control" id="founded" name="founded" />
+                    </div>
                     <input className="btn btn-primary" type="submit" value="Add Place" />
                 </form>
             </main>
