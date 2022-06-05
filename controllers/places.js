55e2dcac4a8fd1aff8bd92bf9750160efc898e77
Euri Tanaka <dev@mugendiary.com>
2022-06-04 20:34:24 -0700

    Added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -9,6 +9,7 @@ router.get('/', (req, res) => {
     res.render('places/index', {places})
 })
 
+
 router.get('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -18,36 +19,74 @@ router.get('/:id', (req, res) => {
   } else {
     res.render('places/show', {place: places[id], id})
   }
-
+  
 })
 
 router.post('/', (req, res) => {
-    if (!req.body.pic) {
-      // Default image if not provided
-      req.body.pic = 'http://placekitten.com/400/400'
-    }
-    if (!req.body.city) {
-      req.body.city = 'Anytown'
-    }
-    if (!req.body.state) {
-      req.body.state = 'USA'
-    }
-    places.push(req.body)
-    res.redirect('/places')
-  })
+  if (!req.body.pic) {
+    // Default image if not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
 
-  router.delete('/places/:id', (req, res) => {
-    let id = Number(req.params.id)
-    if (isNaN(id)) {
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    places.splice(id, 1)
+    res.redirect('/places')
+  }
+})
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/edit', {place: places[id], id})
+  }
+})
+
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
       res.render('error404')
-    }
-    else if (!places[id]) {
+  }
+  else if (!places[id]) {
       res.render('error404')
-    }
-    else {
-      res.send('STUB DELETE places/:id')
-    }
-  })
-  
+  }
+  else {
+      if (!req.body.pic) {
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
+
+
 
 module.exports = router
